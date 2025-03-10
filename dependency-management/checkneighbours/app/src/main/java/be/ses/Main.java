42d010a677399ab78516101d606e32e52fa1748b
WarreDidden <warre.didden@telenet.be>
2025-03-10 18:33:47 +0100

    opdracht 5 done

---
+++ dependency-management/checkneighbours/app/src/main/java/be/ses/Main.java
@@ -0,0 +1,16 @@
+package be.ses;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Iterable<Integer> grid = Arrays.asList(
+            0, 0, 1, 0,
+            1, 1, 0, 2,
+            2, 0, 1, 3,
+            0, 1, 1, 1
+        );
+        
+        System.out.println(CheckNeighboursInGrid.getSameNeighboursIds(grid, 4, 4, 5));
+    }
+}
